59b29b0ca32c11cab73ca0d7cbcc33148411c863
Ramsy Anton <ramsyanton@Ramsys-MacBook-Air.local>
2023-07-05 19:25:22 -0700

    x

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -12,3 +12,14 @@
 
 # Input: nums = [1,1,1,3,3,4,3,2,4,2]
 # Output: true
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
